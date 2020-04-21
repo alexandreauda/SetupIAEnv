483515c2b9b840b719722aeb11a380049543a346
alexandreauda <alexandreauda@yahoo.fr>
2020-04-21 21:50:36 +0200

    Add files via upload

---
+++ SetupIAEnv_Folder_Windows/InstallAnaconda.bat
@@ -0,0 +1,4 @@
+@echo off
+REM @echo off if you don't want to see the commands executed.
+REM Shortcut to launch the LaunchSetupIaenv.exe
+CALL .\LaunchSetupIaenv\LaunchSetupIaenv\bin\x64\Release\LaunchSetupIaenv.exe
